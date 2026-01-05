bf8701b7183519ffe0c716cd01db89f225c69f51
Wafa D <wafadaghbari27@gmail.com>
2026-01-05 02:30:20 +0100

    test: invalid input handling

--- src/test/java/com/wafa/tennis/domain/TennisGameTest.java
+++ src/test/java/com/wafa/tennis/domain/TennisGameTest.java
@@ -1,4 +1,21 @@
 package com.wafa.tennis.domain;
 
+import com.wafa.tennis.FakeScorePrinter;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
+
 class TennisGameTest {
+
+    @Test
+    void should_throw_exception_when_sequence_contains_invalid_character() {
+        // Given
+        var printer = new FakeScorePrinter();
+        var tennisGame = new TennisGame(printer);
+
+        // When / Then
+        thenThrownBy(() -> tennisGame.play("ABX"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Invalid input");
+    }
 }
