f7f68122e401ca00b66c6a5b42afae18f08c9b16
Wafa D <wafadaghbari27@gmail.com>
2026-01-05 02:26:26 +0100

    feat: implement basic tennis scoring to pass acceptance test

--- src/main/java/com/wafa/tennis/domain/TennisGame.java
+++ src/main/java/com/wafa/tennis/domain/TennisGame.java
@@ -4,7 +4,66 @@ import com.wafa.tennis.domain.port.ScorePrinter;
 
 public final class TennisGame {
 
-    public TennisGame(final ScorePrinter printer) {}
+    private final ScorePrinter scorePrinter;
 
-    public void play(final String sequence) {}
+    private int scoreA;
+    private int scoreB;
+    private boolean gameWon;
+
+    public TennisGame(final ScorePrinter scorePrinter) {
+        this.scorePrinter = scorePrinter;
+    }
+
+    public void play(final String sequence) {
+
+        for (final char ball : sequence.toCharArray()) {
+            if (gameWon) {
+                break;
+            }
+
+            switch (ball) {
+                case 'A' -> handleBallWonByA();
+                case 'B' -> handleBallWonByB();
+                default -> throw new IllegalArgumentException("Invalid input: " + ball);
+            }
+        }
+    }
+
+    private void handleBallWonByA() {
+        if (scoreA == 40) {
+            scorePrinter.print("Player A wins the game");
+            gameWon = true;
+            return;
+        }
+        scoreA = nextScore(scoreA);
+        printCurrentScore();
+    }
+
+    private void handleBallWonByB() {
+        if (scoreB == 40) {
+            scorePrinter.print("Player B wins the game");
+            gameWon = true;
+            return;
+        }
+        scoreB = nextScore(scoreB);
+        printCurrentScore();
+    }
+
+
+    private String formatScore() {
+        return "Player A : " + scoreA + " / Player B : " + scoreB;
+    }
+
+    private void printCurrentScore() {
+        scorePrinter.print(formatScore());
+    }
+
+    private int nextScore(final int currentScore) {
+        return switch (currentScore) {
+            case 0 -> 15;
+            case 15 -> 30;
+            case 30 -> 40;
+            default -> currentScore;
+        };
+    }
 }
