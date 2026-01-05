633153a9ac4e24a0de827f71ab9c9578d10d278d
Wafa D <wafadaghbari27@gmail.com>
2026-01-05 10:22:45 +0100

    test: add unit tests for deuce scenario

--- src/test/java/com/wafa/tennis/domain/TennisGameTest.java
+++ src/test/java/com/wafa/tennis/domain/TennisGameTest.java
@@ -3,6 +3,7 @@ package com.wafa.tennis.domain;
 import com.wafa.tennis.FakeScorePrinter;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 
 class TennisGameTest {
@@ -17,5 +18,40 @@ class TennisGameTest {
         thenThrownBy(() -> tennisGame.play("ABX"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Invalid input");
+    }
+
+    @Test
+    void should_print_deuce_when_both_players_reach_40() {
+        // Given
+        var printer = new FakeScorePrinter();
+        var tennisGame = new TennisGame(printer);
+
+        // When
+        tennisGame.play("ABABAB");
+
+        // Then
+        then(printer.outputs())
+                .containsExactly(
+                        "Player A : 15 / Player B : 0",
+                        "Player A : 15 / Player B : 15",
+                        "Player A : 30 / Player B : 15",
+                        "Player A : 30 / Player B : 30",
+                        "Player A : 40 / Player B : 30",
+                        "Deuce"
+                );
+    }
+
+    @Test
+    void should_display_standard_scores_for_equalities_before_40() {
+        var printer = new FakeScorePrinter();
+        var tennisGame = new TennisGame(printer);
+        tennisGame.play("ABAB");
+
+        then(printer.outputs())
+                .last()
+                .isEqualTo("Player A : 30 / Player B : 30");
+
+        then(printer.outputs())
+                .doesNotContain("Deuce");
     }
 }
